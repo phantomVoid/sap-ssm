f073604427bec558bb2cc205c280ccc2ec686d62
phantomVoid <phantomsaber@foxmail.com>
2019-08-23 15:32:05 +0800

    nothing

--- src/main/java/com/phantom/sap/intf/ItemLevelIntf.java
+++ src/main/java/com/phantom/sap/intf/ItemLevelIntf.java
@@ -1,6 +1,5 @@
 package com.phantom.sap.intf;
 import javax.jws.WebService;
-import java.util.Map;
 
 @WebService(name = "ItemLevelIntf", targetNamespace = "http://cxf.temptation.cn/")
 public interface ItemLevelIntf {
