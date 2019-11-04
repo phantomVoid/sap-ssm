801953df57593181786c77d9f597e24f31694f0b
phantomVoid <phantomsaber@foxmail.com>
2019-11-04 10:27:11 +0800

    POJO

---
+++ src/main/java/com/phantom/pojo/MapRes.java
@@ -0,0 +1,32 @@
+package com.phantom.pojo;
+
+/**
+ * Map Res
+ * @author:phantomsaber
+ * @version:2019/11/2 23:47
+ * @email:phantomsaber@foxmail.com
+ **/
+public class MapRes {
+    public Boolean flag;
+
+    public String desc;
+
+    public MapRes() {
+    }
+
+    public Boolean getFlag() {
+        return flag;
+    }
+
+    public void setFlag(Boolean flag) {
+        this.flag = flag;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
